e28ad9e44ff67eafabadc376469dbc28adf14c31
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2023-01-23 12:22:04 -0800

    Add variable for secret key and generated new secret key

--- backend/pet_care_exchange/settings.py
+++ backend/pet_care_exchange/settings.py
@@ -11,7 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+
+load_dotenv()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mbu)y*4q7rhd*w)41+_u2@^mb5&8lw45+97ofqysi#^17-6-oc'
 
+#generated new key and put in .env since original was exposed to github
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
