fd48991ca456ed6a91ffb1fe45c1ccf02385e034
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2023-02-05 00:05:57 -0800

    Comment Template view in URL for deployment

--- pet_care_exchange/urls.py
+++ pet_care_exchange/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-# from petsitters import views
-# from pets import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,4 +32,4 @@ urlpatterns = [
 
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
